9e64712498c21daa9a8ab7e72fc818676ef6300c
Jacob Madrid <madridjacobr@gmail.com>
2016-10-03 17:10:16 -0700

    implemented function that checks ALL substrings and returns a dict

--- rabinkarp.py
+++ rabinkarp.py
@@ -1,6 +1,5 @@
 # RABIN-KARP IMPLEMENTATION
 # TODO: maybe come up with a better hash() than Python's own
-# TODO: maybe figure out how to do all the comparisons in one without iterating through a list
 
 def isSubstringPresent(pattern, test, caseSensitive = True):
     if not caseSensitive:
@@ -20,7 +19,6 @@ def isSubstringPresent(pattern, test, caseSensitive = True):
         return False
 
 def firstOccurrenceOfSubstringIfPresent(patterns, test, caseSensitive = True):
-    substringPresence = { }
     if not caseSensitive:
         test = test.lower()
         patterns = [pattern.lower() for pattern in patterns]
@@ -35,8 +33,31 @@ def firstOccurrenceOfSubstringIfPresent(patterns, test, caseSensitive = True):
         stepCounter+=1
     return False
 
+def areSubstringsPresentOnePass(patterns, test, caseSensitive = True):
+    substringPresence = { }
+    if not caseSensitive:
+        test = test.lower()
+        patterns = [pattern.lower() for pattern in patterns]
+    patternHashes = [hash(pattern) for pattern in patterns]
+    stepCounter = 0
+    while stepCounter < len(test):
+        for i in xrange(0, len(patterns), 1):
+            testHash = hash(test[stepCounter:stepCounter+len(patterns[i])])
+            if patternHashes[i] == testHash:
+                if patterns[i] == test[stepCounter:stepCounter+len(patterns[i])]:
+                    substringPresence[patterns[i]] = stepCounter
+            if stepCounter == len(patterns[i]) and patterns[i] not in substringPresence:
+                substringPresence[patterns[i]] = -1
+        stepCounter+=1
+    return substringPresence
+
 def areSubstringsPresent(patterns, test, caseSensitive = True):
     substrings = []
     for pattern in patterns:
         substrings.append(isSubstringPresent(pattern, test, caseSensitive))
     return substrings
+
+test = "googgooggooogogoogoapplegoogobeegogooogoo"
+patterns = ["goa", "apple", "gobee", "bee", "gooogoogo"]
+
+mycheck = areSubstringsPresentOnePass(patterns, test)
